0a4c34f334f60cd7a9b8ac20a4dba0b0149dc17d
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-01-04 01:25:23 +0530

    Time: 1 ms (14.71%) | Memory: 42.5 MB (11.76%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,20 @@
 class Solution {
-    int[][] dp;
 
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        for (int i = 0; i < m; i++)
-            Arrays.fill(dp[i], -1);
-        return traverse(m - 1, n - 1);
-    }
+        int[][] dp = new int[m][n];
 
-    public int traverse(int i, int j) {
-        if (i < 0 || j < 0)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (i == 0 && j == 0) {
+        dp[0][0] = 1;
 
-            return 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0 && j==0)continue;
+                int up = (i > 0) ? dp[i - 1][j] : 0;
+                int left = (j > 0) ? dp[i][j - 1] : 0;
+                dp[i][j] = up + left;
+            }
         }
+        return dp[m - 1][n - 1];
 
-        return dp[i][j] = traverse(i - 1, j) + traverse(i, j - 1);
     }
+
 }
