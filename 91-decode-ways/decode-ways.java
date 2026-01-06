2bd3add7570115becefa369c0aaf51b14289c374
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-01-07 00:55:15 +0530

    Time: 1 ms (85.28%) | Memory: 43.5 MB (15.81%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,24 +1,45 @@
 class Solution {
-    int[] dp;
+
     public int numDecodings(String s) {
-        dp=new int[s.length()];
-        Arrays.fill(dp,-1);
-        return decode(s,0);
-    }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        if (s.charAt(0) == '0')
+            return 0;
+        dp[n] = 1;
 
-    public int decode(String s,int i){
-        if(i==s.length())return 1;
-        if(s.charAt(i)=='0')return 0;
-        if(dp[i]!=-1)return dp[i];
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            dp[i] = dp[i + 1];
 
-        int ways=decode(s,i+1);
-
-        if((i+1)<s.length()){
-            int num=Integer.parseInt(s.substring(i,i+2));
-            if(num>=10 && num<=26)ways+=decode(s,i+2);
+            if ((i + 1) < n) {
+                int num = Integer.parseInt(s.substring(i, i + 2));
+                if (num >= 10 && num <= 26)
+                    dp[i] += dp[i + 2];
+            }
         }
-        return dp[i]=ways;
 
-
+        return dp[0];
     }
+
+    // public int decode(String s, int i) {
+    //     if (i == s.length())
+    //         return 1;
+    //     if (s.charAt(i) == '0')
+    //         return 0;
+    //     if (dp[i] != -1)
+    //         return dp[i];
+
+    //     int ways = decode(s, i + 1);
+
+    //     if ((i + 1) < s.length()) {
+    //         int num = Integer.parseInt(s.substring(i, i + 2));
+    //         if (num >= 10 && num <= 26)
+    //             ways += decode(s, i + 2);
+    //     }
+    //     return dp[i] = ways;
+
+    // }
 }
