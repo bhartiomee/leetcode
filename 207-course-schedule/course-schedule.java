8bb5ac0d2244ab4cd50ef5855cc7dac41bd99186
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-01-09 00:44:56 +0530

    Time: 7 ms (63.12%) | Memory: 47.3 MB (36.46%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,40 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        Queue<Integer> q = new LinkedList<>();
+    public boolean canFinish(int n, int[][] prerequisites) {
+        int[] indegree = new int[n];
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-
-        for (int[] p : prerequisites) {
-            int course = p[0];
-            int prereq = p[1];
-
-            adj.get(prereq).add(course);
-            indegree[course]++;
+        for (int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj.get(u).add(v);
         }
 
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < n; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
             if (indegree[i] == 0)
                 q.offer(i);
         }
-        int cnt = 0;
+        int idx = 0;
         while (!q.isEmpty()) {
             int node = q.poll();
-            cnt++;
-
+            idx++;
             for (int it : adj.get(node)) {
                 indegree[it]--;
-                if(indegree[it] == 0)q.offer(it);
+                if (indegree[it] == 0)
+                    q.offer(it);
             }
-
         }
 
-        return cnt == numCourses;
-    }
+        return idx == n;
 
+    }
 }
