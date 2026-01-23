76476e732c61f5b3659d3f5b3833bb7bc2de6210
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-01-24 00:33:44 +0530

    Time: 1 ms (71.20%) | Memory: 43.4 MB (83.36%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,32 +1,47 @@
 class Solution {
-    int[][] memo;
+   
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        memo=new int[n+1][2];
+         int[][] dp=new int[n+2][2];
 
-        for(int[]row:memo){
-            Arrays.fill(row,-1);
+        dp[n][0]=0;
+        dp[n][1]=0;
+        dp[n+1][0]=0;
+        dp[n+1][1]=0;
+
+        for(int i=n-1;i>=0;i--){
+
+            //hasStock=0    
+            int buy=-prices[i]+dp[i+1][1];
+            int skip=dp[i+1][0];
+            dp[i][0]=Math.max(buy,skip);
+
+            //hasStock=1
+            int sell=prices[i]+dp[i+2][0];
+            int skipWithoutStock=dp[i+1][1];
+            dp[i][1]=Math.max(sell,skipWithoutStock);
+
         }
-        return calcProfit(prices, 0, 0);
-        //  return ans;
+        return dp[0][0];
+      
     }
 
-    public int calcProfit(int[] prices, int idx, int hasStock) {
-        if (idx >= prices.length)
-            return 0;
+    // public int calcProfit(int[] prices, int idx, int hasStock) {
+    //     if (idx >= prices.length)
+    //         return 0;
 
-        if(memo[idx][hasStock]!=-1)return memo[idx][hasStock];    
+    //     if(memo[idx][hasStock]!=-1)return memo[idx][hasStock];    
 
-        if (hasStock==0) {
-            int buy = -1 * prices[idx] + calcProfit(prices, idx + 1, 1);
-            int skip = calcProfit(prices, idx + 1, hasStock);
-            return memo[idx][hasStock]=Math.max(buy, skip);
-        } else {
-            int sell = prices[idx] + calcProfit(prices, idx + 2, 0);
-            int skip = calcProfit(prices, idx + 1, hasStock);
+    //     if (hasStock==0) {
+    //         int buy = -1 * prices[idx] + calcProfit(prices, idx + 1, 1);
+    //         int skip = calcProfit(prices, idx + 1, hasStock);
+    //         return memo[idx][hasStock]=Math.max(buy, skip);
+    //     } else {
+    //         int sell = prices[idx] + calcProfit(prices, idx + 2, 0);
+    //         int skip = calcProfit(prices, idx + 1, hasStock);
 
-            return memo[idx][hasStock]= Math.max(sell, skip);
-        }
+    //         return memo[idx][hasStock]= Math.max(sell, skip);
+    //     }
 
-    }
+    // }
 }
