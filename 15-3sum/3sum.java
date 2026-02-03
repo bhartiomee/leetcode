41d95b65034796e8dc2662c9fb365d999bd1e260
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-02-03 23:53:13 +0530

    Time: 36 ms (32.61%) | Memory: 59.3 MB (42.83%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,28 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        int n=nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
 
-        for(int i=0;i<n-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < n; i++) {
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j = i + 1;
+            int k = n - 1;
+            int toFind = -1 * nums[i];
 
-            int num1=nums[i];
-            
-            int j=i+1;
-            int k=n-1;
-           
-            while(j<k){
-                int sum=nums[j]+nums[k];
-                if(sum==(-1*num1)){
-                    ans.add(Arrays.asList(num1,nums[j],nums[k]));
+            while (j < k) {
+                int sum = nums[j] + nums[k];
+                if (sum == toFind) {
+                    ans.add(new ArrayList
+                            (Arrays.asList(nums[i], nums[j], nums[k])));
+                    while (j + 1 < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (k - 1 > j && nums[k] == nums[k - 1])
+                        k--;
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (sum < toFind) {
+                    j++;
+                } else {
+                    k--;
                 }
-                else if(sum<(-1*num1))j++;
-                else k--;
+
             }
         }
         return ans;
