735e6fd4222a16446eddc98b4c961a1f435be34b
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-02-26 02:16:30 +0530

    Time: 7 ms (40.08%) | Memory: 45.2 MB (68.60%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,29 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-
+    List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        helper(candidates, 0, target, new ArrayList<>());
+        helper(candidates,target,0,new ArrayList<>());
         return ans;
     }
 
-    public void helper(int[] candidates, int idx, int target, List<Integer> tmp) {
-        if (target == 0) {
-            ans.add(new ArrayList(tmp));
+    public void helper(int[] candidates, int target,int idx,List<Integer> tmp){
+        if(target==0){
+            ans.add(new ArrayList<>(tmp));
             return;
         }
-        if (idx >= candidates.length || target < 0)
-            return;
+
+        if(idx>=candidates.length || target<0)return;
 
         tmp.add(candidates[idx]);
-        helper(candidates, idx + 1, target - candidates[idx], tmp);
-        tmp.remove(tmp.size() - 1);
+        helper(candidates,target-candidates[idx],idx+1,tmp);
 
-        while (idx + 1 < candidates.length 
-                                        && candidates[idx] == candidates[idx + 1])
+        tmp.remove(tmp.size()-1);
+
+        while(idx+1<candidates.length && candidates[idx]==candidates[idx+1]){
             idx++;
-        helper(candidates, idx + 1, target, tmp);
+        }
+
+        helper(candidates,target,idx+1,tmp);
+
     }
 }
