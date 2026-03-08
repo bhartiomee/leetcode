4484c91f223ce71a3b90ab144322584cfe1098d1
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-03-09 00:55:29 +0530

    Time: 26 ms (19.73%) | Memory: 58.6 MB (6.44%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -4,25 +4,22 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         memo = new int[amount + 1][n];
-        for (int[] row:memo) {
+        for (int[] row : memo) {
             Arrays.fill(row, -1);
         }
-        return dfs(amount, coins, 0);
+
+        return dp(amount, coins, 0);
     }
 
-    public int dfs(int amount, int[] coins, int idx) {
+    public int dp(int amount, int[] coins, int idx) {
         if (amount == 0)
             return 1;
-        if (amount < 0)
+        if (amount < 0 || idx >= coins.length)
             return 0;
-        if (idx == coins.length)
-            return 0;
-
         if (memo[amount][idx] != -1)
             return memo[amount][idx];
-        int ways = 0;
 
-        ways += dfs(amount - coins[idx], coins, idx) + dfs(amount, coins, idx + 1);
-        return memo[amount][idx] = ways;
+        return memo[amount][idx] = dp(amount - coins[idx], coins, idx) +
+                dp(amount, coins, idx + 1);
     }
 }
