087c21862ca4ec1afddb6f3ccd2cc67bd78918c3
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-03-10 00:37:56 +0530

    Time: 2 ms (99.14%) | Memory: 44.4 MB (74.82%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-         int n=str.length();
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i)))
+                i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j)))
+                j--;
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
 
-        for(int i=0;i<n;i++){
-            if(str.charAt(i)!=str.charAt(n-i-1))return false;
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
     }
 
-    
 }
