76d1c6c8f2286e914e8192c7f2aff19cfadb368e
Omee Bharti <57210394+bhartiomee@users.noreply.github.com>
2026-05-10 17:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (94.88%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,34 @@
 class Solution {
-    List<List<Integer>> ans=new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        helper(nums,new ArrayList<>(),new boolean[nums.length]);
+        func(nums, 0);
         return ans;
     }
 
-    public void helper(int[] nums,List<Integer> tmp,boolean[] used){
-
-        if(tmp.size()==nums.length){
-            ans.add(new ArrayList(tmp));
+    public void func(int[] nums, int idx) {
+        if (idx == nums.length) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int num : nums) {
+                tmp.add(num);
+            }
+            ans.add(tmp);
             return;
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(used[i])continue;
-            used[i]=true;
-            tmp.add(nums[i]);
-            helper(nums,tmp,used);
-            used[i]=false;
-            tmp.remove(tmp.size()-1);
+        if (idx > nums.length)
+            return;
 
+        for (int i = idx; i < nums.length; i++) {
+            swap(i, idx, nums);
+            func(nums, idx + 1);
+            swap(i, idx, nums);
         }
+    }
+
+    public void swap(int i, int j, int[] nums) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
